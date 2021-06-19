a1ce596775bc0e7a29d4755d1b70eeba56795f0f
Barreiradlc <augustodasilva53@gmail.com>
2021-06-19 18:26:37 -0300

    first commit

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,22 @@
+export interface RepositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string
+    }
+}
+
+function RepositoryItem(props: RepositoryItemProps) {
+    return (
+        <li>
+            <strong>{props.repository?.name ?? "Default"}</strong>
+            <p>{props.repository?.description ?? "Default"}</p>
+
+            <a href={props.repository.html_url}>
+                Acessar repositório
+            </a>
+        </li>
+    )
+}
+
+export { RepositoryItem }
